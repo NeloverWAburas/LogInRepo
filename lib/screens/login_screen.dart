536166fdf2ccbb084bbab7ba7782ce183d6df2bd
FlutLab.io <>
2022-08-14 16:25:54 +0000

    Autosave

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -6,46 +6,58 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(children: [
-Stack(
-        alignment: Alignment.topCenter,
-        children: [
-          Image.asset("assets/images/background.png"),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Image.asset(
-                "assets/images/light-1.png",
-                height: 180,
-              ),
-              Image.asset(
-                "assets/images/light-2.png",
-                height: 100,
-              ),
-              Image.asset("assets/images/clock.png")
-            ],
-          ),
-          Positioned(
-            top: 180,
-            child: Text(
-              "Login",
-              style: TextStyle(
-                  fontSize: 40,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold),
+        body: Column(
+      children: [
+        Stack(
+          alignment: Alignment.topCenter,
+          children: [
+            Image.asset("assets/images/background.png"),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Image.asset(
+                  "assets/images/light-1.png",
+                  height: 180,
+                ),
+                Image.asset(
+                  "assets/images/light-2.png",
+                  height: 100,
+                ),
+                Image.asset("assets/images/clock.png")
+              ],
             ),
-          )
-        ],
-      ),
-       TextField(
-        decoration: InputDecoration(
-          hintText: "Email or Phone number",
-             border: OutlineInputBorder(), 
+            Positioned(
+              top: 180,
+              child: Text(
+                "Login",
+                style: TextStyle(
+                    fontSize: 40,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
+              ),
+            )
+          ],
         ),
-      ), 
-      ],)
-      
-      
-    );
+        Form(
+          child: Container(
+            margin: EdgeInsets.symmetric(horizontal: 20),
+            child: Column(children: [
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: "Email or Phone number",
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: "Password",
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ]),
+          ),
+        )
+      ],
+    ));
   }
 }
