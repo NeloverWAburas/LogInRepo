67a1ab9e35bb028d02139f9edc71d6828a36095c
FlutLab.io <>
2022-08-14 12:03:29 +0000

    Autosave

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        alignment: Alignment.topCenter,
+        children: [
+        Image.asset("assets/images/background.png"),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Image.asset("assets/images/light-1.png"),
+            Image.asset("assets/images/light-2.png"),
+            Image.asset("assets/images/clock.png")
+          ],
+        ),
+      ],
+      ),
+    );
+  }
+}
