fb3373e2a3bc147b8c40d1d71583e83acdc3c94b
FlutLab.io <>
2022-08-14 17:27:20 +0000

    Autosave

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -83,7 +83,7 @@ class LoginScreen extends StatelessWidget {
                     begin: Alignment.topRight,
                     end: Alignment.bottomLeft,
                     colors: [
-                      Colors(0xff517bca),
+                      Color(0xffaabbdb),
                       Color(0xff517bca),
                     ],
                   ),
